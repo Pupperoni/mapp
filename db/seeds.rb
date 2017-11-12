312d18cbd42faa4de436a5f21b32d9d90c886495
Adel Mandanas <mandanas.adel@gmail.com>
2017-11-12 22:37:28 +0800

    Parse the other datasets

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,55 @@
 require 'csv'
 
 ######################################################################
-# ACCIDENTS SECTION
+# ACCIDENTS SECTION                                                  #
 ######################################################################
-
-# accidents = File.read(Rails.root.join('lib', 'seeds', 'accidents', '2008 CAAP Aircraft Incidents.csv'))
-# accidents = CSV.parse(accidents, :headers => true, :encoding => 'ISO-8859-1')
-#
-# accidents.each do |row|
-#   t = Location.new
-#   t.city = row['place_of_occurance']
-#   t.save
-# end
 
 Dir.foreach('./lib/seeds/accidents') do |item|
   next if item == '.' or item == '..'
 
-  csv_text = File.read(Rails.root.join('lib', 'seeds', 'accidents', item))
-  csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+  # csv_text = File.read(Rails.root.join('lib', 'seeds', 'accidents', item))
+  # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+  #
+  # csv.each do |row|
+  #   t = Location.new
+  #   t.city = row['place_of_occurance']
+  #   t.save
+  # end
 
-  csv.each do |row|
-    t = Location.new
-    t.city = row['place_of_occurance']
-    t.save
-  end
+end
+
+######################################################################
+# CARGO AND FREIGHT SECTION                                          #
+######################################################################
+
+Dir.foreach('./lib/seeds/cargo-freight') do |item|
+  next if item == '.' or item == '..'
+
+  # csv_text = File.read(Rails.root.join('lib', 'seeds', 'cargo-freight', item))
+  # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+end
+
+######################################################################
+# INFRASTRUCTURE                                                     #
+######################################################################
+
+Dir.foreach('./lib/seeds/infrastructure') do |item|
+  next if item == '.' or item == '..'
+
+  # csv_text = File.read(Rails.root.join('lib', 'seeds', 'infrastructure', item))
+  # csv = CSV.parse(csv_text.scrub, :headers => true, :encoding => 'ISO-8859-1:utf-8')
+
+end
+
+######################################################################
+# ATMs                                                               #
+######################################################################
+
+Dir.foreach('./lib/seeds/atms') do |item|
+  next if item == '.' or item == '..'
+
+  # csv_text = File.read(Rails.root.join('lib', 'seeds', 'atms', item))
+  # csv = CSV.parse(csv_text.scrub, :headers => true, :encoding => 'ISO-8859-1:utf-8')
 
 end
