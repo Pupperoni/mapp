e90a150300ea21221b1e990a2ad7830820224d4c
Adel Mandanas <mandanas.adel@gmail.com>
2017-11-12 20:19:18 +0800

    Fix multiple primary key error

--- db/migrate/20171112053058_create_locations.rb
+++ db/migrate/20171112053058_create_locations.rb
@@ -1,12 +1,11 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
-    create_table :locations do |t|
+    create_table :locations, primary_key: [:region, :province, :city] do |t|
       t.integer :region
       t.string :province
       t.string :city
 
       t.timestamps
     end
-    execute "ALTER TABLE words ADD PRIMARY KEY (region,province,city);"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171112053058) do
+
+  create_table "locations", primary_key: ["region", "province", "city"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "region", null: false
+    t.string "province", null: false
+    t.string "city", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
