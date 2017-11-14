fa568dac40919acdf2c2c334b2deb30cd48a56ea
Adel Mandanas <mandanas.adel@gmail.com>
2017-11-14 21:11:11 +0800

    Fully populate the tables related to Accidents

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -1,3 +1,3 @@
 class Situation < ApplicationRecord
-  belongs_to :airport
+  belongs_to :airport, optional: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,52 +19,51 @@ Dir.foreach('./lib/seeds/accidents') do |item|
   csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
   csv.each do |row|
-    # if Location.exists?(city: row['place_of_occurance'])
-    #   loc = Location.where(city: row['place_of_occurance']).first.id
-    # else
-    #   t = Location.new
-    #   t.city = row['place_of_occurance']
-    #   t.save
-    #   loc = t.id
-    # end
-    #
-    # if Aircraft.exists?(aircraft_registration: row['aircraft_registration_or_model'])
-    #   reg = Aircraft.where(aircraft_registration: row['aircraft_registration_or_model']).first.id
-    # else
-    #   t = Aircraft.new
-    #   t.aircraft_registration = row['aircraft_registration_or_model']
-    #   t.craft_type = row['aircraft_type']
-    #   t.save
-    #   reg = t.id
-    # end
-    #
-    # t = Situation.new
-    # t.accident_type = row['type_of_occurance']
-    # t.report_type = row['report']
-    # t.status = row['status']
-    # t.save
-    # acc = t.id
-    # puts acc
-  #
-  #   t = HasAccident.new
-  #   t.aircraft_id = reg
-  #   # t.situation_id = acc
-  #   t.save
-  #
-  #   t = HappenedAt.new
-  #   # t.situation_id = acc
-  #   t.location_id = loc
-  #   t.save
-  #
-  #   t = HappenedOn.new
-  #   t.accident_date = row['date']
-  #   #t.situation_id = acc
-  #   t.save
-  #
-  #   t = HasCasualty.new
-  #   #t.situation_id = acc
-  #   t.save
-  #
+    if Location.exists?(location: row['place_of_occurance'])
+      loc = Location.where(location: row['place_of_occurance']).first.id
+    else
+      t = Location.new
+      t.location = row['place_of_occurance']
+      t.save
+      loc = t.id
+    end
+
+    if Aircraft.exists?(aircraft_registration: row['aircraft_registration_or_model'])
+      reg = Aircraft.where(aircraft_registration: row['aircraft_registration_or_model']).first.id
+    else
+      t = Aircraft.new
+      t.aircraft_registration = row['aircraft_registration_or_model']
+      t.craft_type = row['aircraft_type']
+      t.save
+      reg = t.id
+    end
+
+    t = Situation.new
+    t.accident_type = row['type_of_occurance']
+    t.report_type = row['report']
+    t.status = row['status']
+    t.save
+    acc = t.id
+
+    t = HasAccident.new
+    t.aircraft_id = reg
+    t.situation_id = acc
+    t.save
+
+    t = HappenedAt.new
+    t.situation_id = acc
+    t.location_id = loc
+    t.save
+
+    t = HappenedOn.new
+    t.accident_date = row['date']
+    t.situation_id = acc
+    t.save
+
+    t = HasCasualty.new
+    t.situation_id = acc
+    t.save!
+
   end
 
 end
@@ -80,11 +79,11 @@ Dir.foreach('./lib/seeds/cargo-freight') do |item|
   csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
   csv.each do |row|
-    if Location.exists?(city: row['airport'])
-      loc = Location.where(city: row['airport']).first.id
+    if Location.exists?(location: row['airport'])
+      loc = Location.where(location: row['airport']).first.id
     else
       t = Location.new
-      t.city = row['airport']
+      t.location = row['airport']
       t.save
       loc = t.id
       t = Airport.new
@@ -136,62 +135,62 @@ Dir.foreach('./lib/seeds/infrastructure') do |item|
   csv = CSV.parse(csv_text.scrub, :headers => true, :encoding => 'ISO-8859-1:utf-8')
 
   csv.each do |row|
-    if Location.exists?(city: row['project_location'])
-      loc = Location.where(city: row['project_location']).first.id
-    else
-      t = Location.new
-      t.city = row['project_location']
-      t.save
-      loc = t.id
-      t = Project.new
-      t.pcost = row['project_cost']
-      t.pdesc = row['project_description']
-      t.save
-      proj = t
-    end
-
-    if ImplementingOffice.exists?(office_name: row['implementing_office'])
-      imof = ImplementingOffice.where(office_name: row['airline_operator']).first.id
-    else
-      t = ImplementingOffice.new
-      t.office_name = row['implementing_office']
-      t.save
-      imof = t.id
-    end
-
-    if Contractor.exists?(cname: row['project_contractor'])
-      imof = Contractor.where(cname: row['project_contractor']).first.id
-    else
-      t = Contractor.new
-      t.cname = row['project_contractor']
-      t.save
-      contr = t.id
-    end
-
-    t = StartedOn.new
-    t.project_start_date = row['contract_start_date']
-    t.projects_id = proj
-    t.save
-
-    t = ImplementedBy.new
-    t.projects_id = proj
-    t.implementing_offices_id = imof
-    t.save
-
-    t = HandledBy.new
-    t.projects_id = proj
-    t.contractors_id = contr
-    t.save
-
-    t = BuiltIn.new
-    t.projects_id = proj
-    t.locations_id = loc
-    t.save
-
-    t = CompletedBy.new
-    t.projects_id = proj
-    t.project_end_date = row['actual_completion_year']+'/'+row['actual_completion_month']+'/01 00.00.00'
-    t.save!
+    # if Location.exists?(location: row['project_location'])
+    #   loc = Location.where(location: row['project_location']).first.id
+    # else
+    #   t = Location.new
+    #   t.location = row['project_location']
+    #   t.save
+    #   loc = t.id
+    #   t = Project.new
+    #   t.pcost = row['project_cost']
+    #   t.pdesc = row['project_description']
+    #   t.save
+    #   proj = t
+    # end
+    #
+    # if ImplementingOffice.exists?(office_name: row['implementing_office'])
+    #   imof = ImplementingOffice.where(office_name: row['airline_operator']).first.id
+    # else
+    #   t = ImplementingOffice.new
+    #   t.office_name = row['implementing_office']
+    #   t.save
+    #   imof = t.id
+    # end
+    #
+    # if Contractor.exists?(cname: row['project_contractor'])
+    #   imof = Contractor.where(cname: row['project_contractor']).first.id
+    # else
+    #   t = Contractor.new
+    #   t.cname = row['project_contractor']
+    #   t.save
+    #   contr = t.id
+    # end
+    #
+    # t = StartedOn.new
+    # t.project_start_date = row['contract_start_date']
+    # t.projects_id = proj
+    # t.save
+    #
+    # t = ImplementedBy.new
+    # t.projects_id = proj
+    # t.implementing_offices_id = imof
+    # t.save
+    #
+    # t = HandledBy.new
+    # t.projects_id = proj
+    # t.contractors_id = contr
+    # t.save
+    #
+    # t = BuiltIn.new
+    # t.projects_id = proj
+    # t.locations_id = loc
+    # t.save
+    #
+    # t = CompletedBy.new
+    # t.projects_id = proj
+    # t.project_end_date = row['actual_completion_year']+'/'+row['actual_completion_month']+'/01 00.00.00'
+    # t.save!
 
   end
 
